9987c7185a330b6fe689bc758d2405bcbd4feb6f
Uigeol <gamoon777@naver.com>
2025-06-12 16:01:28 +0900

    chore: 초기 설정

---
+++ src/main/java/com/oopsw/seongsubean_frontend/SeongsubeanFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.oopsw.seongsubean_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeongsubeanFrontendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SeongsubeanFrontendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/oopsw/seongsubean_frontend/SeongsubeanFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oopsw.seongsubean_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeongsubeanFrontendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
