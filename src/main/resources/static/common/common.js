7a222868749c1f947c133b3d3378b75434f08fe2
Uigeol <gamoon777@naver.com>
2025-06-17 16:32:44 +0900

    feat: url 전역변수, 로그아웃 구현

--- src/main/resources/static/common/common.js
+++ src/main/resources/static/common/common.js
@@ -1,20 +1,26 @@
 import {updateAuthDisplay} from '/common/header.js';
+const rootUrl = 'http://127.0.0.1:8881';
 
 const common = axios.create({
-  baseURL: 'http://127.0.0.1:8881',
+  baseURL: rootUrl,
   headers: {
     'Content-Type': 'application/json'
   }
 });
-export default common;
 
-export async function loadLayout() {
+async function loadLayout() {
   // Header와 Footer를 순차적으로 로드
   await loadFragment('header-container', '/common/header.html');
   await loadFragment('footer-container', '/common/footer.html');
 
   // HTML 로드 완료 후 인증 상태 업데이트
   updateAuthDisplay();
+
+  // 로그아웃 버튼 이벤트 리스너 추가
+  document.getElementById('btn-logout').addEventListener('click', () => {
+    console.log('토큰 삭제')
+    localStorage.removeItem('auth');
+  });
 }
 
 async function loadFragment(containerId, filePath) {
@@ -29,3 +35,5 @@ async function loadFragment(containerId, filePath) {
     console.error(error);
   }
 }
+
+export {common, rootUrl, loadLayout}
